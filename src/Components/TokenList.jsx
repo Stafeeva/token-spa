00f10de855128abe44ab20700ae9becba4febbf1
Natalia Stafeeva <n.stafeeva@gmail.com>
2019-07-28 16:11:33 +0200

    Add buttons to Token List

--- src/Components/TokenList.jsx
+++ src/Components/TokenList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import autobind from 'autobind-decorator';
+import { Row, Col, Button } from 'antd';
 
 import SearchField from './SearchField.jsx';
 
@@ -8,13 +9,32 @@ class TokenList extends Component {
     console.log('filtering...', filter);
   }
 
+  @autobind onClickIssueToken() {
+    console.log('clicked Issue Token');
+  }
+
+  @autobind onClickExportToCSV() {
+    console.log('clicked Export To CSV');
+  }
+
+
   render () {
-    const { filterTokens } = this;
+    const { filterTokens, onClickExportToCSV, onClickIssueToken } = this;
 
     return (
       <div>
         <h1>Token List</h1>
-        <SearchField onType={filterTokens}/>
+        <Row gutter={16}>
+          <Col span={18}>
+            <SearchField onType={filterTokens}/>
+          </Col>
+          <Col span={3}>
+            <Button type="primary" onClick={onClickIssueToken}>Issue Token</Button>
+          </Col>
+          <Col span={3}>
+            <Button type="primary" onClick={onClickExportToCSV}>Export To CSV</Button>
+          </Col>
+        </Row>
       </div>
     );
   }
